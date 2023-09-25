e7dbe5b4e49eb61981527dea711de9ebfde1907e
sadiqmayari <sadiqmayari@gmail.com>
2023-09-25 12:34:38 +0500

    push

---
+++ app.js
@@ -0,0 +1,49 @@
+//declare variable username
+
+var userName;
+
+// daclare variable myName and assign string
+var myName = "Muhammad Sadiq";
+
+// declare variable message and assign string and display alert
+var message;
+message = "Hello World";
+alert(message);
+
+// write script and show in alert boxes
+
+var sirName = "Jhone Doe";
+var age = 15;
+var qualification = "Certified Mobile App Developer";
+
+alert(sirName);
+alert(age + " years old");
+alert(qualification);
+
+var itemName = "PIZZA";
+var message = "";
+
+for (var i = itemName.length; i >= 1; i--) {
+  message += itemName.slice(0, i) + "\n";
+}
+
+alert(message);
+
+
+//alert email
+
+var email = "My Email Address is sadiqmayari@gmail.com";
+
+alert(email);
+
+// variable book in a sencetece to alert
+
+var book = "A smarter way to learn JavaScript";
+
+alert("I am Trying to learn from a Book " + book);
+
+// show design in alert
+
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+
+alert(design)
